d904cead164d5c7bb5def84dbabb4fff9f2777d8
Gustavo Henrique Dias <31208960+GustavoDiias@users.noreply.github.com>
2019-10-03 13:55:15 -0300

    API

---
+++ APIOrientacao/APIOrientacao.Data/Context/Contexto.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APIOrientacao.Data.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto()
+        { }
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        { }
+
+        public DbSet<Pessoa> Pessoa { get; set; }
+        public DbSet<Aluno> Aluno { get; set; }
+        public DbSet<Curso> Curso { get; set; }
+        public DbSet<Projeto> Projeto { get; set; }
+        public DbSet<Situacao> Situacao { get; set; }
+        public DbSet<SituacaoProjeto> SituacaoProjeto { get; set; }
+
+        //public void PessoaMB(ModelBuilder modelBuilder)
+        //{
+
+        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Remover a propriedade de restrição-validação de exclusão
+                // em cascata
+            foreach (var relacionamento in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys()))
+            {
+                relacionamento.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.ForSqlServerUseIdentityColumns();
+            modelBuilder.HasDefaultSchema("dbo");
+
+            modelBuilder.Entity<Pessoa>(e =>
+            {
+                e.ToTable("Pessoa");
+                e.HasKey(c => c.IdPessoa).HasName("IdPessoa");
+                e.Property(c => c.IdPessoa).HasColumnName("IdPessoa")
+                .ValueGeneratedOnAdd();
+
+                e.Property(c => c.Nome).HasColumnName("Nome")
+                .IsRequired()
+                .HasMaxLength(300);
+            });
+
+            modelBuilder.Entity<Curso>(e =>
+            {
+                e.ToTable("Curso");
+                e.HasKey(c => c.IdCurso).HasName("IdCurso");
+                e.Property(c => c.IdCurso).HasColumnName("IdCurso")
+                    .ValueGeneratedOnAdd();
+
+                e.Property(c => c.Nome).HasColumnName("Nome")
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+            });
+
+            modelBuilder.Entity<Aluno>(e =>
+            {
+                e.ToTable("Aluno");
+                e.HasKey(c => c.IdPessoa).HasName("IdPessoaAluno");
+                e.Property(c => c.IdPessoa).HasColumnName("IdPessoa")
+                    .IsRequired();
+                e.Property(c => c.IdCurso).HasColumnName("IdCurso")
+                    .IsRequired();
+
+                e.Property(c => c.Matricula)
+                .HasColumnName("Matricula")
+                .IsRequired()
+                .HasMaxLength(8);
+
+                e.Property(c => c.RegistroAtivo)
+                .HasColumnName("RegistroAtivo")
+                .IsRequired();
+
+                e.HasOne(d => d.Pessoa)
+                    .WithOne(p => p.Aluno)
+                    .HasForeignKey<Aluno>(d => d.IdPessoa)
+                    .HasConstraintName("PFK_PessoaAluno");
+
+                e.HasOne(d => d.Curso)
+                    .WithMany(p => p.Alunos)
+                    .HasForeignKey(d => d.IdCurso)
+                    .HasConstraintName("FK_CursoAluno");
+            });
+
+            modelBuilder.Entity<Situacao>(e =>
+            {
+                e.ToTable("Situacao");
+                e.HasKey(c => c.IdSituacao).HasName("IdSituacao");
+                e.Property(c => c.IdSituacao).HasColumnName("IdSituacao")
+                    .ValueGeneratedOnAdd();
+
+                e.Property(c => c.Descricao).HasColumnName("Descricao")
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            modelBuilder.Entity<Projeto>(e =>
+            {
+                e.ToTable("Projeto");
+                e.HasKey(k => k.IdProjeto).HasName("IdProjeto");
+                e.Property(c => c.IdProjeto).HasColumnName("IdProjeto")
+                    .ValueGeneratedOnAdd();
+
+                e.Property(c => c.IdPessoa).HasColumnName("IdPessoa")
+                .IsRequired();
+
+                e.Property(c => c.Encerrado).HasColumnName("Encerrado")
+                    .IsRequired();
+                e.Property(c => c.Nota).HasColumnName("Nota").IsRequired();
+
+                e.Property(c => c.Nome).HasColumnName("Nome").IsRequired().
+                HasMaxLength(200);
+
+                e.HasOne(d => d.Aluno)
+                    .WithMany(p => p.Projetos)
+                    .HasForeignKey(d => d.IdPessoa)
+                    .HasConstraintName("FK_AlunoProjeto");
+            });
+
+            modelBuilder.Entity<SituacaoProjeto>(e =>
+            {
+                e.ToTable("SituacaoProjeto");
+                e.HasKey(k => new { k.IdProjeto, k.IdSituacao })
+                .HasName("FK_SituacaoProjeto");
+
+                e.Property(c => c.IdProjeto).HasColumnName("IdProjeto")
+                .IsRequired();
+
+                e.Property(c => c.IdSituacao).HasColumnName("IdSituacao")
+                .IsRequired();
+
+                e.Property(c => c.DataRegistro).HasColumnName("DataRegistro")
+                .HasColumnType("datetime").IsRequired();
+
+                e.HasOne(d => d.Situacao)
+                    .WithMany(p => p.SituacoesProjeto)
+                    .HasForeignKey(d => d.IdSituacao)
+                    .HasConstraintName("FK_SituacaoSituacaoProjeto");
+
+                e.HasOne(d => d.Projeto)
+                    .WithMany(p => p.SituacoesProjeto)
+                    .HasForeignKey(d => d.IdProjeto)
+                    .HasConstraintName("FK_ProjetoSituacaoProjeto");
+            });
+        }
+    }
+}
